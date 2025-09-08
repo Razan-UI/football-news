6fc639cee3f8a175e195f12ee6fd7b1ebfe713b6
Razan MS <razan.muhammad@ui.ac.id>
2025-09-08 18:48:36 +0700

    feat: initiate project

--- main/models.py
+++ main/models.py
@@ -1,32 +1,27 @@
 import uuid
 from django.db import models
 
-class News(models.Model):
+
+
+class Product(models.Model):
     CATEGORY_CHOICES = [
-        ('transfer', 'Transfer'),
-        ('update', 'Update'),
-        ('exclusive', 'Exclusive'),
-        ('match', 'Match'),
-        ('rumor', 'Rumor'),
-        ('analysis', 'Analysis'),
+        ('clothing', 'Clothing'),
+        ('tools', 'Tools'),
+        ('shoes', 'Shoes'),
     ]
     
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(max_length=255)
-    content = models.TextField()
-    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='update')
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    description = models.TextField()
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='shirt')
     thumbnail = models.URLField(blank=True, null=True)
-    news_views = models.PositiveIntegerField(default=0)
-    created_at = models.DateTimeField(auto_now_add=True)
     is_featured = models.BooleanField(default=False)
     
     def __str__(self):
-        return self.title
+        return self.title 
     
-    @property
-    def is_news_hot(self):
-        return self.news_views > 20
-        
+    @property        
     def increment_views(self):
         self.news_views += 1
         self.save()
